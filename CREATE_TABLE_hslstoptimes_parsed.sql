109b978a9e5452ab61041922e452b99d4c536c66
Markus <36296293+markusvalo@users.noreply.github.com>
2018-02-09 22:56:53 +0200

    Add files via upload

---
+++ CREATE_TABLE_hslstoptimes_parsed.sql
@@ -0,0 +1,269 @@
+DROP TABLE IF EXISTS public.hslstoptimes_tram CASCADE;
+
+CREATE TABLE public.hslstoptimes_tram AS 
+ SELECT 
+    hslstops.stop_name,
+    hslstops.stop_desc,
+    hslstops.stop_lon AS stop_lon_float, 
+    hslstops.stop_lat AS stop_lat_float,
+    hslstops.zone_id,
+    hslstops.vehicle_type,
+    hslroutes.route_short_name,
+    hslroutes.route_long_name,
+    hslroutes.route_desc,
+    hsltrips.trip_headsign,
+    hslstop_times.stop_headsign,
+    split_part(hsltrips.trip_id, '_', 2) AS trip_YYYYMMDD_txt,
+    left(split_part(hsltrips.trip_id, '_', 2),4) AS trip_YYYY_txt,
+    substring(split_part(hsltrips.trip_id, '_', 2),5,2) AS trip_MM_txt,
+    right(split_part(hsltrips.trip_id, '_', 2),2) AS trip_DD_txt,
+    split_part(hsltrips.trip_id, '_', 3) AS trip_weekday_txt,
+    hsltrips.trip_id,
+    to_timestamp(hslstop_times.arrival_time, 'HH24:MI:SS'::text)::time without time zone AS arrival_time,
+    to_timestamp(hslstop_times.departure_time, 'HH24:MI:SS'::text)::time without time zone AS departure_time,
+    hslstop_times.arrival_time AS arrival_time_txt,
+    hslstop_times.departure_time AS departure_time_txt,
+    hslstop_times.stop_sequence,
+    hslroutes.route_type,
+    hsltrips.direction_id,
+    hslstops.location_type,
+    ST_SetSRID(ST_Point(hslstops.stop_lon, hslstops.stop_lat), 4326) AS stop_point
+ FROM hslstop_times,
+    hslstops,
+    hsltrips,
+    hslcalendar,
+    hslroutes
+ WHERE hslstop_times.stop_id = hslstops.stop_id AND 
+	hsltrips.service_id = hslcalendar.service_id AND 
+	hsltrips.trip_id = hslstop_times.trip_id AND 
+	hslroutes.route_id = hsltrips.route_id AND 
+	hslcalendar.wednesday = 1 AND 
+	split_part(hsltrips.trip_id, '_', 2) = '20180205' AND 
+	hslstops.vehicle_type = 0;
+
+
+
+DROP TABLE IF EXISTS public.hslstoptimes_metro CASCADE;
+
+CREATE TABLE public.hslstoptimes_metro AS 
+ SELECT 
+    hslstops.stop_name,
+    hslstops.stop_desc,
+    hslstops.stop_lon AS stop_lon_float, 
+    hslstops.stop_lat AS stop_lat_float,
+    hslstops.zone_id,
+    hslstops.vehicle_type,
+    hslroutes.route_short_name,
+    hslroutes.route_long_name,
+    hslroutes.route_desc,
+    hsltrips.trip_headsign,
+    hslstop_times.stop_headsign,
+    split_part(hsltrips.trip_id, '_', 2) AS trip_YYYYMMDD_txt,
+    left(split_part(hsltrips.trip_id, '_', 2),4) AS trip_YYYY_txt,
+    substring(split_part(hsltrips.trip_id, '_', 2),5,2) AS trip_MM_txt,
+    right(split_part(hsltrips.trip_id, '_', 2),2) AS trip_DD_txt,
+    split_part(hsltrips.trip_id, '_', 3) AS trip_weekday_txt,
+    hsltrips.trip_id,
+    to_timestamp(hslstop_times.arrival_time, 'HH24:MI:SS'::text)::time without time zone AS arrival_time,
+    to_timestamp(hslstop_times.departure_time, 'HH24:MI:SS'::text)::time without time zone AS departure_time,
+    hslstop_times.arrival_time AS arrival_time_txt,
+    hslstop_times.departure_time AS departure_time_txt,
+    hslstop_times.stop_sequence,
+    hslroutes.route_type,
+    hsltrips.direction_id,
+    hslstops.location_type,
+    ST_SetSRID(ST_Point(hslstops.stop_lon, hslstops.stop_lat), 4326) AS stop_point
+ FROM hslstop_times,
+    hslstops,
+    hsltrips,
+    hslcalendar,
+    hslroutes
+ WHERE hslstop_times.stop_id = hslstops.stop_id AND 
+	hsltrips.service_id = hslcalendar.service_id AND 
+	hsltrips.trip_id = hslstop_times.trip_id AND 
+	hslroutes.route_id = hsltrips.route_id AND 
+	hslcalendar.wednesday = 1 AND 
+	split_part(hsltrips.trip_id, '_', 2) = '20180205' AND 
+	hslstops.vehicle_type = 1 AND
+	left(hslstops.stop_id,1) in ('1', '2');
+
+
+
+DROP TABLE IF EXISTS public.hslstoptimes_train CASCADE;
+
+CREATE TABLE public.hslstoptimes_train AS 
+ SELECT 
+    hslstops.stop_name,
+    hslstops.stop_desc,
+    hslstops.stop_lon AS stop_lon_float, 
+    hslstops.stop_lat AS stop_lat_float,
+    hslstops.zone_id,
+    hslstops.vehicle_type,
+    hslroutes.route_short_name,
+    hslroutes.route_long_name,
+    hslroutes.route_desc,
+    hsltrips.trip_headsign,
+    hslstop_times.stop_headsign,
+    split_part(hsltrips.trip_id, '_', 2) AS trip_YYYYMMDD_txt,
+    left(split_part(hsltrips.trip_id, '_', 2),4) AS trip_YYYY_txt,
+    substring(split_part(hsltrips.trip_id, '_', 2),5,2) AS trip_MM_txt,
+    right(split_part(hsltrips.trip_id, '_', 2),2) AS trip_DD_txt,
+    split_part(hsltrips.trip_id, '_', 3) AS trip_weekday_txt,
+    hsltrips.trip_id,
+    to_timestamp(hslstop_times.arrival_time, 'HH24:MI:SS'::text)::time without time zone AS arrival_time,
+    to_timestamp(hslstop_times.departure_time, 'HH24:MI:SS'::text)::time without time zone AS departure_time,
+    hslstop_times.arrival_time AS arrival_time_txt,
+    hslstop_times.departure_time AS departure_time_txt,
+    hslstop_times.stop_sequence,
+    hslroutes.route_type,
+    hsltrips.direction_id,
+    hslstops.location_type,
+    ST_SetSRID(ST_Point(hslstops.stop_lon, hslstops.stop_lat), 4326) AS stop_point
+ FROM hslstop_times,
+    hslstops,
+    hsltrips,
+    hslcalendar,
+    hslroutes
+ WHERE hslstop_times.stop_id = hslstops.stop_id AND 
+	hsltrips.service_id = hslcalendar.service_id AND 
+	hsltrips.trip_id = hslstop_times.trip_id AND 
+	hslroutes.route_id = hsltrips.route_id AND 
+	hslcalendar.wednesday = 1 AND 
+	split_part(hsltrips.trip_id, '_', 2) = '20180205' AND 
+	hslstops.vehicle_type = 109;
+
+DROP TABLE IF EXISTS public.hslstoptimes_ferry CASCADE;
+
+CREATE TABLE public.hslstoptimes_ferry AS 
+ SELECT 
+    hslstops.stop_name,
+    hslstops.stop_desc,
+    hslstops.stop_lon AS stop_lon_float, 
+    hslstops.stop_lat AS stop_lat_float,
+    hslstops.zone_id,
+    hslstops.vehicle_type,
+    hslroutes.route_short_name,
+    hslroutes.route_long_name,
+    hslroutes.route_desc,
+    hsltrips.trip_headsign,
+    hslstop_times.stop_headsign,
+    split_part(hsltrips.trip_id, '_', 2) AS trip_YYYYMMDD_txt,
+    left(split_part(hsltrips.trip_id, '_', 2),4) AS trip_YYYY_txt,
+    substring(split_part(hsltrips.trip_id, '_', 2),5,2) AS trip_MM_txt,
+    right(split_part(hsltrips.trip_id, '_', 2),2) AS trip_DD_txt,
+    split_part(hsltrips.trip_id, '_', 3) AS trip_weekday_txt,
+    hsltrips.trip_id,
+    to_timestamp(hslstop_times.arrival_time, 'HH24:MI:SS'::text)::time without time zone AS arrival_time,
+    to_timestamp(hslstop_times.departure_time, 'HH24:MI:SS'::text)::time without time zone AS departure_time,
+    hslstop_times.arrival_time AS arrival_time_txt,
+    hslstop_times.departure_time AS departure_time_txt,
+    hslstop_times.stop_sequence,
+    hslroutes.route_type,
+    hsltrips.direction_id,
+    hslstops.location_type,
+    ST_SetSRID(ST_Point(hslstops.stop_lon, hslstops.stop_lat), 4326) AS stop_point
+ FROM hslstop_times,
+    hslstops,
+    hsltrips,
+    hslcalendar,
+    hslroutes
+ WHERE hslstop_times.stop_id = hslstops.stop_id AND 
+	hsltrips.service_id = hslcalendar.service_id AND 
+	hsltrips.trip_id = hslstop_times.trip_id AND 
+	hslroutes.route_id = hsltrips.route_id AND 
+	hslcalendar.wednesday = 1 AND 
+	split_part(hsltrips.trip_id, '_', 2) = '20180205' AND 
+	hslstops.vehicle_type = 4 AND 
+	left(hslstops.stop_id,1)='1';
+
+
+DROP TABLE IF EXISTS public.hslstoptimes_train CASCADE;
+
+CREATE TABLE public.hslstoptimes_train AS 
+ SELECT 
+    hslstops.stop_name,
+    hslstops.stop_desc,
+    hslstops.stop_lon AS stop_lon_float, 
+    hslstops.stop_lat AS stop_lat_float,
+    hslstops.zone_id,
+    hslstops.vehicle_type,
+    hslroutes.route_short_name,
+    hslroutes.route_long_name,
+    hslroutes.route_desc,
+    hsltrips.trip_headsign,
+    hslstop_times.stop_headsign,
+    split_part(hsltrips.trip_id, '_', 2) AS trip_YYYYMMDD_txt,
+    left(split_part(hsltrips.trip_id, '_', 2),4) AS trip_YYYY_txt,
+    substring(split_part(hsltrips.trip_id, '_', 2),5,2) AS trip_MM_txt,
+    right(split_part(hsltrips.trip_id, '_', 2),2) AS trip_DD_txt,
+    split_part(hsltrips.trip_id, '_', 3) AS trip_weekday_txt,
+    hsltrips.trip_id,
+    to_timestamp(hslstop_times.arrival_time, 'HH24:MI:SS'::text)::time without time zone AS arrival_time,
+    to_timestamp(hslstop_times.departure_time, 'HH24:MI:SS'::text)::time without time zone AS departure_time,
+    hslstop_times.arrival_time AS arrival_time_txt,
+    hslstop_times.departure_time AS departure_time_txt,
+    hslstop_times.stop_sequence,
+    hslroutes.route_type,
+    hsltrips.direction_id,
+    hslstops.location_type,
+    ST_SetSRID(ST_Point(hslstops.stop_lon, hslstops.stop_lat), 4326) AS stop_point
+ FROM hslstop_times,
+    hslstops,
+    hsltrips,
+    hslcalendar,
+    hslroutes
+ WHERE hslstop_times.stop_id = hslstops.stop_id AND 
+	hsltrips.service_id = hslcalendar.service_id AND 
+	hsltrips.trip_id = hslstop_times.trip_id AND 
+	hslroutes.route_id = hsltrips.route_id AND 
+	hslcalendar.wednesday = 1 AND 
+	split_part(hsltrips.trip_id, '_', 2) = '20180205' AND 
+	hslstops.vehicle_type = 1;
+
+
+DROP TABLE IF EXISTS public.hslstoptimes_bus CASCADE;
+
+CREATE TABLE public.hslstoptimes_bus AS 
+ SELECT 
+    hslstops.stop_name,
+    hslstops.stop_desc,
+    hslstops.stop_lon AS stop_lon_float, 
+    hslstops.stop_lat AS stop_lat_float,
+    hslstops.zone_id,
+    hslstops.vehicle_type,
+    hslroutes.route_short_name,
+    hslroutes.route_long_name,
+    hslroutes.route_desc,
+    hsltrips.trip_headsign,
+    hslstop_times.stop_headsign,
+    split_part(hsltrips.trip_id, '_', 2) AS trip_YYYYMMDD_txt,
+    left(split_part(hsltrips.trip_id, '_', 2),4) AS trip_YYYY_txt,
+    substring(split_part(hsltrips.trip_id, '_', 2),5,2) AS trip_MM_txt,
+    right(split_part(hsltrips.trip_id, '_', 2),2) AS trip_DD_txt,
+    split_part(hsltrips.trip_id, '_', 3) AS trip_weekday_txt,
+    hsltrips.trip_id,
+    to_timestamp(hslstop_times.arrival_time, 'HH24:MI:SS'::text)::time without time zone AS arrival_time,
+    to_timestamp(hslstop_times.departure_time, 'HH24:MI:SS'::text)::time without time zone AS departure_time,
+    hslstop_times.arrival_time AS arrival_time_txt,
+    hslstop_times.departure_time AS departure_time_txt,
+    hslstop_times.stop_sequence,
+    hslroutes.route_type,
+    hsltrips.direction_id,
+    hslstops.location_type,
+    ST_SetSRID(ST_Point(hslstops.stop_lon, hslstops.stop_lat), 4326) AS stop_point
+ FROM hslstop_times,
+    hslstops,
+    hsltrips,
+    hslcalendar,
+    hslroutes
+ WHERE hslstop_times.stop_id = hslstops.stop_id AND 
+	hsltrips.service_id = hslcalendar.service_id AND 
+	hsltrips.trip_id = hslstop_times.trip_id AND 
+	hslroutes.route_id = hsltrips.route_id AND 
+	hslcalendar.wednesday = 1 AND 
+	split_part(hsltrips.trip_id, '_', 2) = '20180205' AND 
+	hslstops.vehicle_type = 3;
+
+
+
+
---
+++ SELECT_stoptimes_HSL.sql
@@ -0,0 +1,32 @@
+CREATE VIEW hslstoptimes AS
+SELECT 
+  hslstops.stop_name, 
+  hslstops.stop_desc, 
+  POINT(hslstops.stop_lat, hslstops.stop_lon) AS stop_location, 
+  hslstops.zone_id, 
+  hslstops.vehicle_type, 
+  TO_TIMESTAMP(hslstop_times.arrival_time, 'HH24:MI:SS')::TIME AS arrival_time,
+  TO_TIMESTAMP(hslstop_times.departure_time, 'HH24:MI:SS')::TIME AS departure_time,
+  hslroutes.route_short_name, 
+  hslroutes.route_long_name, 
+  hslroutes.route_desc, 
+  hsltrips.trip_headsign, 
+  hsltrips.direction_id, 
+  hslstop_times.stop_headsign, 
+  hslstop_times.stop_sequence, 
+  hslroutes.route_type, 
+  hslstops.location_type
+FROM 
+  public.hslstop_times, 
+  public.hslstops, 
+  public.hsltrips, 
+  public.hslcalendar, 
+  public.hslroutes
+WHERE 
+  hslstop_times.stop_id = hslstops.stop_id AND
+  hsltrips.service_id = hslcalendar.service_id AND
+  hsltrips.trip_id = hslstop_times.trip_id AND
+  hslroutes.route_id = hsltrips.route_id AND
+  hslcalendar.wednesday = 1 AND
+  hsltrips.trip_id LIKE '%20180205_Ke%' AND
+  hslroutes.route_short_name='322' ;
