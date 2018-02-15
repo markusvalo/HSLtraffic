e31038d8d88a9c6ba8428f9fb54d8a9f6e59baae
Markus <36296293+markusvalo@users.noreply.github.com>
2018-02-15 22:35:14 +0200

    Update 1_CREATE_SCHEMA_HSL.sql

--- 1_CREATE_SCHEMA_HSL.sql
+++ 1_CREATE_SCHEMA_HSL.sql
@@ -95,12 +95,7 @@ WITH (
   OIDS = FALSE
 )
 ;
--- upload csv-files, maintained in http://dev.hsl.fi/gtfs/
-COPY HSLstops FROM '~/HSL/stops.txt' DELIMITER ',' CSV HEADER;
-COPY HSLstop_times FROM '~/HSL/stop_times.txt' DELIMITER ',' CSV HEADER;
-COPY HSLtrips FROM '~/HSL/trips.txt' DELIMITER ',' CSV HEADER;
-COPY HSLroutes FROM '~/HSL/routes.txt' DELIMITER ',' CSV HEADER;
-COPY HSLcalendar FROM '~/HSL/calendar.txt' DELIMITER ',' CSV HEADER;
+
 
 
 
